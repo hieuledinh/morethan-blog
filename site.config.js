4ac298b00206afa38ad1da1fca74f8936eec7b80
hieuledinh <119155856+hieuledinh@users.noreply.github.com>
2023-07-25 08:03:05 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Hieu Le Dinh",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar2.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Student",
     bio: "Edit by Notion & Vercel",
     email: "info.leehieu@gmail.com",
